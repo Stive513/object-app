90927cc48bbb7a3ac1f329ee3614ca62f241320d
Stive513 <stivekamdem113@gmail.com>
2026-04-02 16:46:22 +0100

    Update api.ts

--- HeyamaWeb/web/lib/api.ts
+++ HeyamaWeb/web/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import io from "socket.io-client";
 
-export const API = "https://object-app-1.onrender.com/objects";
+export const API = "https://object-app-1.onrender.com";
 
 export const socket = io(API);
 

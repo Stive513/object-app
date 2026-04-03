44fc2d132a7e3894badb4db7b374087cdca86b84
Stive513 <stivekamdem113@gmail.com>
2026-04-03 10:42:19 +0100

    Update api.ts

--- HeyamaWeb/web/lib/api.ts
+++ HeyamaWeb/web/lib/api.ts
@@ -1,12 +1,8 @@
 import axios from "axios";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
 const API = process.env.NEXT_PUBLIC_API_URL;
 
-if (!API) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined");
-}
-
-export const socket = io(API, {
-  transports: ["websocket", "polling"],
-});
+export const socket = API
+  ? io(API)
+  : null;

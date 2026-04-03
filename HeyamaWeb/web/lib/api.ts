c2c2362e0a45d36c47b54585f55ffa6e2b138ceb
Stive513 <stivekamdem113@gmail.com>
2026-04-03 10:48:52 +0100

    Update api.ts

--- HeyamaWeb/web/lib/api.ts
+++ HeyamaWeb/web/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const API = process.env.NEXT_PUBLIC_API_URL;
+export const API = process.env.NEXT_PUBLIC_API_URL;
 
 export const socket = API
   ? io(API)
